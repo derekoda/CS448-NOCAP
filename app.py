f55bca19fda3f5935cf5ce27cfaf209de18b0df2
HiddenHentai <m201806h@gmail.com>
2023-02-21 13:33:05 -0700

    Changed frontend folder to base name templates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from pets import pets
 app = Flask(__name__)
 
 
@@ -12,12 +12,23 @@ def index():
 def about():
     par = "<p>This is a simple paragraph with text that will be rendered to a page <br> with out an html file</p>"
     button = '''<button onclick="/">Press Me to go Back</button><script>function myFunction() {
-        location.replace("https://www.w3schools.com")
+        location.replace('{/templates/index.html}')
         }</script>'''
 
-    combined = par + button
+    combined =  par + button
     return combined
 
+@app.route('/data/<pet_type>')
+def datapage(pet_type):
+
+    html = f'<h1>List of {pet_type}</h1>'
+    html += "<ul>"
+    for pet in pets.get(pet_type,[]):
+        html += f"<li>{pet['name']}</li>"
+    html += '</ul>'
+
+
+    return html
 
 if __name__ == '__main__':
     app.run(debug= True)

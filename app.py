ce4230b8cbb323b79808d185e8ee4a4463b0f2c9
HiddenHentai <m201806h@gmail.com>
2023-02-21 19:24:10 -0700

    added comments

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, url_for
 from pets import pets
+
 app = Flask(__name__)
 
-
+#Route used to render a HTML file that can be edited in the templates folder.
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
+#An Example route to show you how you can use HTML in Flask instead.
 @app.route('/about')
 def about():
     par = "<p>This is a simple paragraph with text that will be rendered to a page <br> with out an html file</p>"
@@ -30,12 +31,17 @@ def datapage(pet_type):
     html += '</ul>'
     return html
 
+#Passes a url requesting the data page followed by the animal and it's ID
+#Try inputting in manually and see how it displays.
+#ex) http://127.0.0.1:5000/data/dogs/1
 @app.route('/data/<string:pet_type>/<int:pet_id>')
 def pet(pet_type, pet_id):
     pet_list = pets.get(pet_type)
     pet = pet_list[pet_id]
     first = " Is the pet number "
     end = " in the list."
+    #Render HTMl using Flask instead of using an HTML file.
+    # You can also contcat strings and add database data through here. 
     return f'''<h1>{pet['name']} {first} {pet_id} {end}</h1>
         <img src="{pet['url']}"><p>{pet['description']}</p><ul><li>{pet['breed']}</li><li>{pet['age']}</li></ul>'''
 

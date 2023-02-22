7f4ec72f3edd8dbdd1fd14682923d7fa0e66ea4d
HiddenHentai <m201806h@gmail.com>
2023-02-21 19:19:43 -0700

    added a few more examples for data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from pets import pets
 app = Flask(__name__)
 
@@ -23,12 +23,23 @@ def datapage(pet_type):
 
     html = f'<h1>List of {pet_type}</h1>'
     html += "<ul>"
-    for pet in pets.get(pet_type,[]):
-        html += f"<li>{pet['name']}</li>"
+
+    for i, pet in enumerate(pets.get(pet_type,[])):
+        html += f'''<li><a href="{url_for('pet', pet_type=pet_type, pet_id=i)}"{pet['name']}</a></li>'''
+        i += 1
     html += '</ul>'
-
-
     return html
+
+@app.route('/data/<string:pet_type>/<int:pet_id>')
+def pet(pet_type, pet_id):
+    pet_list = pets.get(pet_type)
+    pet = pet_list[pet_id]
+    first = " Is the pet number "
+    end = " in the list."
+    return f'''<h1>{pet['name']} {first} {pet_id} {end}</h1>
+        <img src="{pet['url']}"><p>{pet['description']}</p><ul><li>{pet['breed']}</li><li>{pet['age']}</li></ul>'''
+
+
 
 if __name__ == '__main__':
     app.run(debug= True)

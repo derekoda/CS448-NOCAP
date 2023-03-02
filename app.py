fcc4851415b47f399e1b7908d8a03ff9648d70ed
derekoda <doda@nmsu.edu>
2023-03-02 12:16:17 -0700

    Fixed page render issue

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-#Route used to render a HTML file that can be edited in the templates folder.
+# Routes used to render a HTML file that can be edited in the templates folder.
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template("index.html")
+
 @app.route('/about')
 def about():
     return render_template("about.html")
+
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
 @app.route('/courseplanning')
 def courseplanning():
     return render_template("courseplanning.html")
+
 @app.route('/degreeflow')
 def degreeflow():
     return render_template("degreeflow.html")

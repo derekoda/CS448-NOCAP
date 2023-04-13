b4daa8876b5afd2d60e3032f3317bdfe35c76879
Derek Oda <doda@psl.nmsu.edu>
2023-04-13 11:24:08 -0600

    Still working course menu

--- scheduleGenerator.py
+++ scheduleGenerator.py
@@ -6,7 +6,25 @@ def generateSchedule(coursesTaken):
 
 
     result = cursor.execute("SELECT * FROM schedules")
-    print(set(result.fetchall()) & set(coursesTaken))
-    return (set(result.fetchall()) & set(coursesTaken))
+
+    deficiencySet = set([])
+
+    for row in result:
+        for course in coursesTaken:
+            if row[1] not in course[0]:
+                deficiencySet.add(row[1])
+            if row[2] not in course[0]:
+                deficiencySet.add(row[2])
+            if row[3] not in course[0]:
+                deficiencySet.add(row[3])
+            if row[4] not in course[0]:
+                deficiencySet.add(row[4])
+
+    print(coursesTaken)   
+    print('\n-----------------\n')
+    print(deficiencySet)
+
+
+    #return (set(result.fetchall()) & set(coursesTaken))
 
     

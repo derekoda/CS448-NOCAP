5a9ee749d3b8025588c4d0871cde89a7f588b52c
HiddenHentai <m201806h@gmail.com>
2023-05-01 19:06:59 -0600

    Changes to htmlScraper and other files

--- htmlScraper.py
+++ htmlScraper.py
@@ -1,29 +1,59 @@
 from bs4 import BeautifulSoup
 import html5lib
-
+import re #used for matching any number or text on the html 
 
 # htmlScraper takes in a string of html and returns a string of text
 def htmlScraper(html):
     soup = BeautifulSoup(html, 'html5lib')
-    
+
+    #Regex matches any class with the string matching the html class
+    regex = re.compile(r'requirement\s+S(.*)')
+
     # find all elements with class 'takenCourse' and store them in a list
     course_elements = soup.find_all('tr', class_='takenCourse')
-    
+    course_req_ele = soup.find_all('div', class_=regex)
     # create a list of lists to store the course, description, and grade
     global course_list 
     course_list = []
-    
+    course_req = []
     # iterate through the list of course elements and print the course and description
     for course_element in course_elements:
         course = course_element.find('td', class_='course')
         grade = course_element.find('td', class_='grade')
         description = course_element.find('td', class_='description')
         
-        # append the course, description, and grade to the course_list
-        course_list.append([course.text.strip().replace(' E', '').replace(' ', ''), description.text.strip().split('CONVERTED',1)[0].strip()]) #, grade.text.strip()])
+        # append the course, description,    course_elements = soup.find_all('tr', class_='takenCourse')
+        course_list.append([course.text.strip().replace(' E', '').replace(' ', ''), description.text.strip().split('CONVERTED',1)[0].strip(), grade.text.strip()])
         
+    #Iterate through the degree requirements    
+    for element in course_req_ele:
+        #Getting the header of the div for course credit hours purposes
+        requiredHeader = element.find('div', class_='reqTitle')
+        
+        print(requiredHeader)
+      
+        #Any already taken courses that have been passed
+        completed_Course = element.find('tr', class_='takenCourse')
+        print(completed_Course)
+        #Now within the taken course check if the courseID was changed and set it to a variable
+        if completed_Course != None: 
+            for element2 in completed_Course:
+                print("IN Loop 2")
+                print(element2)
+                completed_Description= element2.find('td', class_='descLine')
+                #If completed_Description matches string "CONVERTED TO: swap names"
+                if "CONVERTED TO:" in completed_Description:
+                    new_course_name = element2.find('td', class_='course')
+                    new_course_name = completed_Description
+        else:
+            continue
+        not_take_Course = element.find('span', class_='subreqTitle')
+        print(requiredHeader) 
+        print(completed_Course)
+        print(not_take_Course)
     # this is just to verify that the list is being populated correctly
-    print(*course_list, sep = '\n')   
+    #print(*course_list, sep = '\n')   
+    #print(course_req_ele, sep ='\n')
     return course_list
         
    
--- scheduleGenerator.py
+++ scheduleGenerator.py
@@ -4,10 +4,46 @@ def generateSchedule(coursesTaken):
     # establish connection to database
     connection = sqlite3.connect('courselist.db')
     cursor = connection.cursor()
-
+    cursor2 = connection.cursor()
+    print("In COURSE CREATOR!!!!!!!!!!!!")
+    
     # create empty list to store schedules that don't contain the courses taken
     deficiencyList = []
     prereqList = []
+    '''Lets try something different we are getting a list with such elements
+        ['ENGL111G', 'RHETORIC/COMPOSITION', 'B-'],
+        Lets make a new list that compares with the degree plan if the course is already taken
+        then lets not add it to the list, else if the course has a failing grade add to list, else add to list.
+        Once the none taken courses are added to the list we can being creating the courses.
+    '''
+    #Retreive the data from the db
+    cursor2.execute("SELECT * FROM course_list")
+    rows = cursor2.fetchall()
+    #print("This is the bd Row************")
+    #print(rows)
+    #In theory all the data from the db which is not going to be large should be stored in the rows element
+    # No we append it to the plans list with the format we had wanted to 
+    #Making a list of none taken and retaking courses from the degree audit.
+    plan = []
+    #for row in rows:
+     #   plan.append([row[0]])
+
+    #Begining list comparisons, this will check rows list with the coursestaken list if
+    # the Course does not exist in the taken then append to plan list also if the course exists check grade
+    # if the grade is D then add to plan list as this is a failure and will need to be retaken. 
+    for course in rows:
+        #print(course)
+        if course[0] not in [c[0] for c in coursesTaken]:
+            plan.append(course)
+        else:
+            for c in coursesTaken:
+                if c[0] == course[0]:
+                    if c[2] == 'D' and not c[1].startswith('IP'):
+                        plan.append(course)
+                        break
+   #print("Did it work?")
+    #print(coursesTaken)
+    #print(plan)
 
     # build the SQL query dynamically based on the number of courses taken
     query = "SELECT * FROM schedules WHERE "
@@ -30,6 +66,6 @@ def generateSchedule(coursesTaken):
         
         if schedule not in deficiencyList:
             deficiencyList.append(schedule)
-        
+    
     # return the deficiency list
     return deficiencyList
